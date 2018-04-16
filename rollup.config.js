0ee302dd37ad5772d83a7ad4f7599dccfe0ef607
Elijah Meeks <elijahmeeks@gmail.com>
2018-04-15 17:13:35 -0700

    add runtime

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from "rollup-plugin-babel"
 export default {
   input: "src/index.js",
   output: {
-    format: "iife",
+    format: "cjs",
     name: "rough",
     file: "dist/rough.js",
     sourcemap: "inline"
